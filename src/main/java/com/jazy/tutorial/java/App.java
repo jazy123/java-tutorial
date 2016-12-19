5ee638b6e8d8156b9a3f7b5829e4138a465e18c4
jazy123 <zhanyu.jiang@gmail.com>
2016-12-19 15:48:11 +0800

    first submit.

---
+++ src/main/java/com/jazy/tutorial/java/App.java
@@ -0,0 +1,13 @@
+package com.jazy.tutorial.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
